b96f183c863e55b858f3deee443b354d0d53f083
mcdobr <mircea.dobreanu@gmail.com>
2018-06-02 13:55:18 +0300

    Finished problem 16.

---
+++ ds-practice/probG/Time/ClockForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Time
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(116, 124);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 13);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "ClockForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ ds-practice/probG/Time/ClockForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time
+{
+    public partial class ClockForm : Form
+    {
+        private Timer timer; 
+
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClockForm_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += ShowTime;
+            timer.Start();
+
+        }
+
+        private void ShowTime(object sender, EventArgs e)
+        {
+            SystemTime sysTime = new SystemTime();
+            LibWrap.GetSystemTime(sysTime);
+
+            timeLabel.Text = string.Format("{0}:{1}:{2}",
+                formatTimeNumber(sysTime.hour),
+                formatTimeNumber(sysTime.minute),
+                formatTimeNumber(sysTime.second)
+                );
+        }
+
+        private string formatTimeNumber(ushort no)
+        {
+            return (no < 10) ? "0" + no : no.ToString();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class SystemTime
+    {
+        public ushort year;
+        public ushort month;
+        public ushort weekday;
+        public ushort day;
+        public ushort hour;
+        public ushort minute;
+        public ushort second;
+        public ushort millisecond;
+    }
+
+    public class LibWrap
+    {
+        // Declares a managed prototype for the unmanaged function using Platform Invoke.
+        [DllImport("Kernel32.dll")]
+        public static extern void GetSystemTime([In, Out] SystemTime st);
+    }
+}
