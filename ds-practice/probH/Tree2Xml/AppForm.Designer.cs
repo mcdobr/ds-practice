c7a8f326b3baca2eafaa91008d9ab5e1abacd440
mcdobr <mircea.dobreanu@gmail.com>
2018-06-02 22:53:17 +0300

    Finished prob 17.

---
+++ ds-practice/probH/Tree2Xml/AppForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Tree2Xml
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.CheckBoxes = true;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(384, 561);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterCheck);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.saveButton.Location = new System.Drawing.Point(0, 538);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(384, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save to XML";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // importButton
+            // 
+            this.importButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.importButton.Location = new System.Drawing.Point(0, 515);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(384, 23);
+            this.importButton.TabIndex = 2;
+            this.importButton.Text = "Import from XML";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.treeView);
+            this.Name = "AppForm";
+            this.Text = "Tree2Xml";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button importButton;
+    }
+}
+
---
+++ ds-practice/probH/Tree2Xml/AppForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Tree2Xml
+{
+    public partial class AppForm : Form
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "XML files (*.xml)|*xml";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    XDocument xdoc = XDocument.Load(ofd.OpenFile());
+                    xml2tree(xdoc);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = "c:\\";
+            sfd.Filter = "XML files (*.xml)|*xml";
+            sfd.FilterIndex = 1;
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                XDocument xdoc = tree2xml();
+                xdoc.Save(sfd.OpenFile());
+            }
+        }
+
+        private void xml2tree(XDocument xdoc)
+        {
+            treeView.Nodes.Clear();
+
+            XElement xroot = xdoc.Elements().First();
+            TreeNode troot = new TreeNode(stringifyElement(xroot));
+            treeView.Nodes.Add(troot);
+
+            xml2treeStep(xroot.Elements(), troot);
+        }
+
+        
+        private void xml2treeStep(IEnumerable<XElement> elements, TreeNode tparent)
+        {
+            foreach (var elem in elements)
+            {
+                TreeNode tElem = new TreeNode(stringifyElement(elem));
+                xml2treeStep(elem.Elements(), tElem);
+                tparent.Nodes.Add(tElem);
+            }
+        }
+
+        private string stringifyElement(XElement elem)
+        {
+            if (string.IsNullOrWhiteSpace(elem.Value) || elem.HasElements)
+                return elem.Name.ToString();
+            else
+                return string.Format("{0}={1}", elem.Name.ToString(), elem.Value);
+        }
+
+        private XDocument tree2xml()
+        {
+            TreeNode troot = treeView.Nodes[0];
+            XElement xroot = new XElement(troot.Text);
+
+            tree2xmlStep(troot.Nodes, xroot);
+
+            XDocument xdoc = new XDocument();
+            xdoc.Add(xroot);
+            return xdoc;
+        }
+
+        private void tree2xmlStep(TreeNodeCollection elements, XElement xparent)
+        {
+            foreach (TreeNode elem in elements)
+            {
+                XElement xelem = unstringifyElement(elem.Text);
+                tree2xmlStep(elem.Nodes, xelem);
+                xparent.Add(xelem);
+            }
+        }
+
+        private XElement unstringifyElement(string str)
+        {
+            XElement elem = null;
+            if (str.Contains('='))
+            {
+                string[] splitResult = str.Split('=');
+                elem = new XElement(splitResult[0]);
+                elem.Value = splitResult[1];
+            }
+            else
+                elem = new XElement(str);
+            return elem;
+        }
+
+        private void treeView_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            TreeNode telem = e.Node;
+
+
+            treeView.AfterCheck -= treeView_AfterCheck;
+            // Check ancestors
+            TreeNode parent = telem.Parent;
+            while (parent != null)
+            {
+                parent.Checked = parent.Nodes.Cast<TreeNode>().Any(node => node.Checked == true);
+                //parent.Checked = telem.Checked;
+                parent = parent.Parent;
+            }
+
+            // Check descendants
+            checkAllChildren(telem, telem.Nodes);
+
+            treeView.AfterCheck += treeView_AfterCheck;
+        }
+
+        private void checkAllChildren(TreeNode root, TreeNodeCollection descendants)
+        {
+            foreach (TreeNode node in descendants)
+            {
+                node.Checked = root.Checked;
+                checkAllChildren(root, node.Nodes);
+            }
+        }
+    }
+}
