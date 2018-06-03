4a8b1bccc924dfda6981808e7771f1b0bd88199f
mcdobr <mircea.dobreanu@gmail.com>
2018-06-03 15:14:33 +0300

    Terminat prob 20.

---
+++ ds-practice/probK/Config2Xml/AppForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Config2Xml
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(87, 226);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculeaza";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(168, 226);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Salveaza";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "AppForm";
+            this.Text = "Conf2Xml";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ ds-practice/probK/Config2Xml/AppForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Config2Xml
+{
+    public partial class AppForm : Form
+    {
+        private long sum = 0;
+
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // Citeste din fisierul App.config (care in folderul bin/%Configuratie rulare% o sa fie Config2Xml.exe.Config elem appSettings
+            var listOfNumbers = ConfigurationManager.AppSettings["listOfNumbers"].Split(',').Select(x => int.Parse(x)).ToList();
+            sum = listOfNumbers.Sum();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            XDocument xdoc = new XDocument(new XElement("SumOfList", sum));
+            xdoc.Save("output.xml");
+        }
+    }
+}
